303b9d05d3542a3b89a4ed3712fcff1f6f7a22e6
nurelmallah <nourhan.elmallah@gmail.com>
2023-11-14 17:18:12 -0800

    trials

--- executor.c
+++ executor.c
@@ -1,45 +1,42 @@
 #include "shell.h"
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 
 /**
  * execute_command - Executes the given command
  * @args: Array of command and arguments
  *
  * This function checks if the specified command is executable,
- * searches for the command in the PATH, forks a child process,
- * and executes the command in the child process.
+ * forks a child process, and executes the command in the child process.
  */
 void execute_command(char *args[])
-{
-	pid_t child_pid;
-	int status;
 
-	/* Check if the specified command is executable */
+{
 	if (access(args[0], X_OK) == 0)
 	{
-		child_pid = fork();
+		pid_t child_pid = fork();
 
 		if (child_pid == -1)
 		{
 			perror("fork");
+			_exit(EXIT_FAILURE);
 		}
 		else if (child_pid == 0)
 		{
-			/* Child process */
 			execve(args[0], args, NULL);
 			perror("execve");
-			exit(EXIT_FAILURE);
+			_exit(EXIT_FAILURE);
 		}
 		else
 		{
-			/* Parent process */
-			waitpid(child_pid, &status, 0);
+			int status;
+
+			wait(&status);
 		}
 	}
 	else
 	{
-		search_and_execute(args);
+		perror("Command not found");
+		_exit(EXIT_FAILURE);
 	}
 }
